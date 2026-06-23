a9a7b3f5264b9aaccb6115ebeff109cacbc54cc1
rosh <roshan.akthar30@gmail.com>
2026-06-23 18:06:09 +0530

    Add CORS policy

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 const dns = require('node:dns/promises'); 
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
+
+app.use(cors({
+  origin: 'https://workout-buddy-frontend-chi.vercel.app' // Paste your exact Vercel link here!
+}));
 //express app
 const app = express()
 app.use(express.json())
